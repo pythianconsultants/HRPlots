f3d2b7e59fe10b16a3ef65d365ec2aed1c34d711
Pythian Consultants <dhiraj@pythianconsultants.in>
2016-08-29 14:25:44 +0530

    navbarPage

--- server.R
+++ server.R
@@ -6,8 +6,15 @@ shinyServer(function(input, output) {
     p <- ggplot(master2, aes_string(x=input$xvalue))+geom_bar(aes(fill=Status))
     ggplotly(p+theme(axis.text.x=element_blank(),axis.ticks=element_blank()))
 })
+
+
+output$tat <- renderPlotly({
+cnv <- as.data.frame(cnv)
+cnv$Date <- factor(cnv$Date)
+p <- ggplot(cnv, aes(reorder(Date,Count),Count,fill=Date))+geom_bar(stat="identity")+coord_flip()+scale_x_discrete(labels=c("Date of Joinng","Offer Accepted","Offers Approved","Offers Submitted","Interviewed","Reviewed","Screened","Shortlisted"))
+ggplotly(p+xlab(NULL))
 })
 
-ggplot(cnv, aes(reorder(Date,Count),Count,fill=URL))+
-  geom_bar(stat="identity")+coord_flip()
+output$text1 <- renderText({paste("Total number of jobs Released", sum(master$Status=='Released'))})
 
+})
